2fa256e37d43110226b0b1f205b5aba33e125282
AndresC1 <andrescornejolira@gmail.com>
2024-07-19 13:26:32 -0600

    fix: Eliminacion de salidas por consola

--- models/wallet.model.js
+++ models/wallet.model.js
@@ -11,14 +11,12 @@ class WalletModel{
         this.apiUrl = process.env.API_URL;
         WalletModel.instance = this;
         console.log("WalletModel instance created");
-        console.log(this.apiUrl);
     }
 
     callApi(request){
         return new Promise((resolve, reject) => {
             axios.get(this.apiUrl + request.txHash)
                 .then((response) => {
-                    console.log(response)
                     resolve(response.data);
                 }).catch((err) => {
                     reject(err);
